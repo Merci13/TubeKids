58a8eff969a857daaf12e2fd3289fa49877957fa
Merci13 <mrjorxe@gmail.com>
2018-08-21 00:29:16 -0600

    Cambios a la ruta de albums, se agrego el post para guardar un nuevo album

--- TubeKids/routes/album.js
+++ TubeKids/routes/album.js
@@ -11,17 +11,12 @@ var md_upload = multipart({uploadDir : './uploads/album'});
 //rutas de express
 api.get('/album',md_auth.ensureAuth,AlbumController.getAlbum);
 
-//api.post('/album',md_auth.ensureAuth,AlbumController.saveAlbum);
+api.post('/album',md_auth.ensureAuth,AlbumController.saveAlbum);
 
-//api.get('/albums/:page?',md_auth.ensureAuth,AlbumController.getAlbums);
 
-//api.put('/album/:id',md_auth.ensureAuth,AlbumController.updateAlbum);
 
-//api.delete('/album/:id',md_auth.ensureAuth,AlbumController.deleteAlbum);
 
-//api.post('/upload-image-album/:id',[md_auth.ensureAuth,md_upload],AlbumController.uploadImage);
 
-//api.get('./get-image-album/:imageFile',AlbumController.getImageFile)
 
 //exporta las rutas
 module.exports = api;
