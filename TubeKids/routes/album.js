9b6aecfc3d63dfd074d7a857b395304b4020c90c
Merci13 <mrjorxe@gmail.com>
2018-08-21 09:49:15 -0600

    se agreegaron comentarios a las rutas del albums

--- TubeKids/routes/album.js
+++ TubeKids/routes/album.js
@@ -9,9 +9,9 @@ var multipart = require('connect-multiparty');
 var md_upload = multipart({uploadDir : './uploads/album'});
 
 //rutas de express
-api.get('/album',md_auth.ensureAuth,AlbumController.getAlbum);
+api.get('/album',md_auth.ensureAuth,AlbumController.getAlbum);//metodo para obtner los albums
 
-api.post('/album',md_auth.ensureAuth,AlbumController.saveAlbum);
+api.post('/album',md_auth.ensureAuth,AlbumController.saveAlbum);//metodo para guardar un album
 
 
 
