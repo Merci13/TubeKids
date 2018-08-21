d239b9d47cd673f4f878d7696fdb3887ff37903b
Merci13 <mrjorxe@gmail.com>
2018-08-21 00:24:49 -0600

    cambios al album controller

--- TubeKids/controllers/album.js
+++ TubeKids/controllers/album.js
@@ -16,8 +16,35 @@ function getAlbum(req, res) {
 
 };
 
+//metodo para guardar un album
+function saveAlbum(req,res){
+    var album = new Album();
+    var params = req.body;
 
+    album.title = params.title;
+    album.description = params.description;
+    album.year = params.year;
+    album.image = 'null';
+    album.Artist = params.artist;
+    
 
+    album.save((err,albumStored)=>{
+        if (err) {
+            res.status(500).send({message: "Error en el servidor"});
+        } else {
+            if (!albumStored) {
+                res.status(404).send({message:"No se ha guardado el album"});
+            }else{
+                res.status(200).send({albumStored});
+
+            }
+        }
+    });
+
+}
+
+//metodo para exportar los metodos dentro de este hoja
 module.exports = {
-    getAlbum
+    getAlbum,
+    saveAlbum
 }
